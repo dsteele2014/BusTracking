d031533f2ed65f34f0a36cb13628eec9dbfab80e
dsteele2014 <dsteele4444@gmail.com>
2018-02-20 12:36:02 -0500

    Add code to update Json

--- BusTracking/server.js
+++ BusTracking/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const app = express()
+const jsonWriter = require('./public/scripts/writeToJson.js');
+const app = express();
+
 app.use(express.static('public'));
 app.set('view engine','ejs')
 app.get('/', function (req, res) {
   res.render('index');
-})
-
+});
+app.post('/updateJson',function(req,res){
+    jsonWriter.writeToJSON();
+    res.write("Json updated");
+    res.end();
+});
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('App listening on port 3000')
+});
